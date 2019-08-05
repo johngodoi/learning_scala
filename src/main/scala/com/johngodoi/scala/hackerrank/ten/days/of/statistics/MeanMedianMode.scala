13d5417bbde800433ad4ace238c17114eed034d9
John Henrique Teixeira de Godoi <jgodoi1@its.jnj.com>
2019-08-05 20:33:00 -0300

    part a of day 0 for 10 days of statistics

---
+++ src/main/scala/com/johngodoi/scala/hackerrank/ten/days/of/statistics/MeanMedianMode.scala
@@ -0,0 +1,25 @@
+package com.johngodoi.scala.hackerrank.ten.days.of.statistics
+object MeanMedianMode {
+
+  def main(args: Array[String]): Unit = {
+    val qtt = scala.io.StdIn.readInt()
+    var array = Array[Int]()
+    val numbers = scala.io.StdIn.readLine().split(" ")
+    for(i <- 0 until qtt ){
+      array = numbers(i).toInt +: array
+    }
+    val mean = array.sum.toDouble / array.length.toDouble
+    println(f"$mean%1.1f")
+    val aSorted = array.sorted
+    if ((array.length % 2) != 0)
+      println(aSorted(array.length/2))
+    else {
+      val median = (aSorted(array.length / 2).toDouble + aSorted((array.length / 2) - 1).toDouble) / 2
+      println(f"$median%1.1f")
+    }
+    val countElements = array.map(el => (el,1)).groupBy(pair => pair._1).map(pair => (pair._1,pair._2.map(p => p._2).sum))
+    val maxCount = countElements.values.max
+    println(countElements.filter(pair => pair._2>=maxCount).min._1)
+  }
+}
+
